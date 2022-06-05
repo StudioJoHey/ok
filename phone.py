4132c4582d232033d8fdd9fad197fe9e7aaae273
OKA_JoHey_NUC <OKA_JoHey@studio-johey.de>
2022-06-05 23:56:41 +0200

    commited

---
+++ phone.py
@@ -0,0 +1,42 @@
+from pyfirmata import Arduino, util 
+from time import sleep
+
+board = Arduino("/dev/ttyACM0", baudrate=57600)
+
+#print(board.get_firmata_version()) 
+# #checks generell woking connection
+
+flagIncomingFromVirtual = 0
+flagHoererOben = 0
+
+# Start iterator to receive input data
+it = util.Iterator(board)
+it.start()
+board.analog[0].enable_reporting()
+
+while True:
+    sleep(0.01)
+    it = util.Iterator(board)
+    it.start()
+    board.analog[0].enable_reporting()
+    readResult = board.analog[0].read()
+    #print(readResult)
+    if readResult <= 0.2:
+        flagHoererOben = 1
+    else:
+        flagHoererOben = 0
+    sleep(0.2)
+
+    print("flagHoererOben = " + str(flagHoererOben))
+
+while flagIncomingFromVirtual == 1:
+
+    for x in range(10):
+        board.digital[2].write(1)
+        sleep(0.03)
+        board.digital[2].write(0)
+        board.digital[3].write(1)
+        sleep(0.03)
+        board.digital[3].write(0)
+
+    sleep(2)
