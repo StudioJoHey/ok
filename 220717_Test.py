b43bde6af9808f8d10571057a1618a99e5a65113
JoHey im Zug <whatever>
2022-07-23 13:08:42 +0200

    im zug

--- 220717_Test.py
+++ 220717_Test.py
@@ -6,13 +6,13 @@ import websockets
 
 import json
 
-from pyfirmata import Arduino, util
+#from pyfirmata import Arduino, util
 from time import sleep
 from api_model.EventObject import EventObject
 
 
 # board = Arduino("/dev/ttyACM0", baudrate=57600)
-board: Optional[Arduino] = None
+board = None
 
 # print(board.get_firmata_version())
 # #checks generell woking connection
@@ -30,13 +30,14 @@ data = '{"event": "RingsMaybe"}'
 WsDomain = "localhost"
 WsPort = 3003
 
-StopEvent = threading.Event()
+RingStopEvent = threading.Event()
+UpdateStopEvent = threading.Event()
 
 
 # switches the pysical phone bell on by alternating the current
 def ring() -> None:
     print("ring init")
-    while not StopEvent.is_set():  # when run as "event based" subprocess
+    while not RingStopEvent.is_set():  # when run as "event based" subprocess
         print("ring")
         if board is not None:
             for x in range(10):
@@ -50,40 +51,67 @@ def ring() -> None:
         sleep(1)
 
 
+# switches the pysical phone bell on by alternating the current
+async def continous_update(websocket: websockets.WebSocketServerProtocol) -> None:
+    
+    while not UpdateStopEvent.is_set():  # when run as "event based" subprocess
+        print("updating hoerer state")
+        await websocket.send("abc")
+
+        sleep(1)
+
+
 # return flagIncomingFromVirtual = "RingingStart"
+
+def get_hoerer_raised() -> bool:
+    return True;
 
 async def handler(websocket: websockets.WebSocketServerProtocol) -> None:
     # loop = asyncio.get_running_loop() #für ThreadPoolExecutor
     ring_thread: threading.Thread
+
+    #update_thread = threading.Thread(target=lambda: continous_update(websocket), name="")
+
+    i=0
     while True:
         print("client connected, awaiting input")
         json_incoming_from_virtual = await websocket.recv()
         try:
-            dict_incoming_from_virtual: EventObject = json.loads(
+            incoming: EventObject = json.loads(
                 json_incoming_from_virtual, object_hook=lambda e: EventObject(e))
         except json.decoder.JSONDecodeError:
-            dict_incoming_from_virtual = json_incoming_from_virtual
+            incoming = json_incoming_from_virtual
 
-        if type(dict_incoming_from_virtual) != EventObject:
+        print("updating hoerer state")
+        i+=1
+        send_future = websocket.send(str(i))
+
+        if type(incoming) != EventObject:
             print("Event isn't a json object: ")
             print(json_incoming_from_virtual)
             continue
 
-        flag_incoming_from_virtual = dict_incoming_from_virtual.event
+        flag_incoming_from_virtual = incoming.event
         print(f'event flag: "{flag_incoming_from_virtual}" ({type(flag_incoming_from_virtual).__name__})')
 
         # ring the bell
         if flag_incoming_from_virtual is None:
             print("json key 'event' missing; can't decide what to do.")
         if flag_incoming_from_virtual == "RingingStart":
-            StopEvent.clear()
+            RingStopEvent.clear()
             ring_thread = threading.Thread(target=ring, name="Ringer")
             ring_thread.start()
         if flag_incoming_from_virtual == "RingingStop":
-            StopEvent.set()
+            RingStopEvent.set()
         else:
             pass  # war als event gedacht, will so aber nicht so richtig
 
+        flag_subscribte_to_hoerer = incoming.subscribe_to_hoerer
+        print(f'subscribe flag: "{flag_subscribte_to_hoerer}" ({type(flag_subscribte_to_hoerer).__name__})')
+        if flag_subscribte_to_hoerer is True:
+            response = dict(hoerer_state=True)
+
+            await websocket.send(json.dumps(response))
 
 async def main() -> None:
     async with websockets.serve(handler, WsDomain, WsPort):
--- api_model/EventObject.py
+++ api_model/EventObject.py
@@ -3,5 +3,6 @@ class EventObject:
     def __init__(self, data: dict):
         if type(data) == dict:
             self.event: str = data.get("event")
+            self.subscribe_to_hoerer = data.get("subscribeToHoerer")
         else:
             self.event = None
